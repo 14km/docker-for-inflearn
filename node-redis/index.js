47a0e9abe758448cb4fd04ed95b3f00bba921e48
K-connor <k.connor614@gmail.com>
2020-11-02 00:40:08 +0900

    어플리케이션 설정하는 파일 만들기!

--- node-redis/index.js
+++ node-redis/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const redis = require('redis');
 
+// 레디스 클라이언트 생성
+const redisClient = redis.createClient({
+  host: 'redis-server',
+  port: 6379,
+});
+
 const app = express();
+
+// 숫자는 0부터 시작한다.
+redisClient.set('number', 0);
+
+app.get('/', (req, res) => {
+  redisClient('number', (err, number) => {
+    // 현재 숫자를 가져온 후 1 씩 증가시킨다.
+    redisClient.set('number', number + 1);
+    res.send(`숫자가 ${number}씩 증가합니다.`);
+  });
+});
 
 app.listen(8080);
 console.log('Server Run!!');
